40c1b57e392a483eea419288fe6a00f16900b4eb
xianhai.su 苏先海 <455098442@qq.com>
2015-07-18 02:46:09 +0800

    spingmvc搭建

---
+++ su/mvn-web/src/main/java/model/User.java
@@ -0,0 +1,63 @@
+package main.java.model;
+
+public class User {
+    private Integer useId;
+
+    private String userName;
+
+    private String password;
+
+    private Integer age;
+
+    private String telphone;
+
+    private String companyName;
+
+    public Integer getUseId() {
+        return useId;
+    }
+
+    public void setUseId(Integer useId) {
+        this.useId = useId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getTelphone() {
+        return telphone;
+    }
+
+    public void setTelphone(String telphone) {
+        this.telphone = telphone == null ? null : telphone.trim();
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName == null ? null : companyName.trim();
+    }
+}
---
+++ su/mvn-web/src/main/java/service/UserService.java
@@ -0,0 +1,40 @@
+package main.java.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import main.java.dao.UserMapper;
+import main.java.model.User;
+
+@Service
+@Transactional
+public class UserService {
+	
+	@Autowired
+	private UserMapper userMapper;
+	
+	public int deleteByPrimaryKey(Integer useId){
+		return userMapper.deleteByPrimaryKey(useId);
+	};
+
+	public int insert(User record){
+    	return userMapper.insert(record);
+    };
+
+    public int insertSelective(User record){
+    	return userMapper.insertSelective(record);
+    };
+
+    public User selectByPrimaryKey(Integer useId){
+    	return userMapper.selectByPrimaryKey(useId);
+    };
+
+    public int updateByPrimaryKeySelective(User record){
+    	return userMapper.updateByPrimaryKeySelective(record);
+    };
+
+    public int updateByPrimaryKey(User record){
+    	return userMapper.updateByPrimaryKey(record);
+    };
+}
