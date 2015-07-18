1e0f9671476ba57c98c4e668194cf8c1511ecbc8
xianhai.su 苏先海 <455098442@qq.com>
2015-07-18 14:43:08 +0800

    springmvc配置

--- su/mvn-web/src/main/java/controller/UserController.java
+++ su/mvn-web/src/main/java/controller/UserController.java
@@ -1,6 +1,5 @@
 package main.java.controller;
 
-import org.codehaus.jackson.map.Module;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,7 @@ public class UserController {
 	UserService userService;
 	
 	@RequestMapping(value = "/index")
-	public String index(Module module){
+	public String index(){
 		System.out.println("start");
 		User user = userService.selectByPrimaryKey(1);
 		System.out.println(user);
