74fd0b9110bbc24bbb72801568f2e93731eaf3a7
xianhai.su 苏先海 <455098442@qq.com>
2015-07-18 15:16:11 +0800

    service添加事务

--- su/mvn-web/src/main/java/service/UserService.java
+++ su/mvn-web/src/main/java/service/UserService.java
@@ -2,10 +2,12 @@ package main.java.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import main.java.dao.UserMapper;
 import main.java.model.User;
 
+@Transactional
 @Service
 public class UserService {
 	
